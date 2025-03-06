96b565b8c542e0957b17c169abe0647cf0cd7e8c
sinhyunsung <hyunsungsung@naver.com>
2025-03-06 11:26:24 +0900

    update: 경량화 샘플

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ document.addEventListener("DOMContentLoaded", function () {
     const group1 = document.getElementById("group1");
     const group2 = document.getElementById("group2");
 
-    const group1Files = ["샘플_기존모델.mp3", "샘플_1안_기존모델축소.mp3", "샘플_2안_경량화.mp3", "샘플_2안_경량화_추가축소.mp3"];
-    const group2Files = ["샘플2_기존모델.mp3", "샘플2_1안_기존모델축소.mp3", "샘플2_2안_경량화.mp3", "샘플2_2안_경량화_추가축소.mp3"];
+    const group1Files = ["샘플_기존모델.mp3", "샘플_1안-기존모델축소.mp3", "샘플_2안-경량화.mp3", "샘플_2안-경량화추가축소.mp3"];
+    const group2Files = ["샘플2_기존모델.mp3", "샘플2_1안-기존모델축소.mp3", "샘플2_2안-경량화.mp3", "샘플2_2안-경량화추가축소.mp3"];
     function cleanFileName(fileName) {
-        return fileName.replace(/^샘플\d*_/u, "").replace(/\.mp3$/i, ""); 
+        return fileName.normalize("NFC")  // Unicode 정규화 적용 (NFD -> NFC)
+                     .replace(/^샘플\d*_/, "")  // "샘플_", "샘플2_" 제거
+                     .replace(/\.mp3$/i, "");  // ".mp3" 확장자 제거
     }
     
     
